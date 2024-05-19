eb95cb4a85aa6ea3a9f3ce71ec33a7dca79552c7
DGRITS <grizay@icloud.com>
2024-05-19 17:21:17 +0300

    todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import TemperatureConverter from './components/TemperatureConverter';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
+      <h1>Конвертор температур:</h1>
       <TemperatureConverter />
+      <h1>Список дел:</h1>
+      <TodoList />
     </div>
   );
 }
