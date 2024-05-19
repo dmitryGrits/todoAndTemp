3426cb1592e1f6357c055d5476511d194c747fc8
DGRITS <grizay@icloud.com>
2024-05-19 16:03:04 +0300

    temperature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TemperatureConverter from './components/TemperatureConverter';
 
 function App() {
   return (
     <div className="App">
-      
+      <TemperatureConverter />
     </div>
   );
 }
